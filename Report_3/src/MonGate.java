9e83589d276e0b1353a5d1b9499f59e91f49c62b
Sigurd <sigurd.thorlund@gmail.com>
2020-11-04 16:23:12 +0100

    added_report_3

---
+++ Report_3/src/MonGate.java
@@ -0,0 +1,19 @@
+//Monitor implementation of Gate (skeleton)
+//Mandatory assignment 3
+//Course 02158 Concurrent Programming, DTU, Fall 2020
+
+//Hans Henrik Lovengreen     Oct 30, 2020
+
+
+public class MonGate extends Gate {
+
+    public void pass() throws InterruptedException {
+    }
+
+    public void open() {
+    }
+
+    public void close() {
+    }
+
+}
