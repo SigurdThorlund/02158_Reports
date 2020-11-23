f77c4616b12d59eac953545e4c64f35f5c91a8cc
Thomas Aamand Witting <thomaswitting97@gmail.com>
2020-11-23 13:15:59 +0100

    Problem 1 - Basic Version

--- Report_4/problem_1/eratosthenes.go
+++ Report_4/problem_1/eratosthenes.go
@@ -8,26 +8,61 @@ package main
 
 import "fmt"
 
-const N = 5
+const N = 15
 
 func odds(out chan<- int) {
-    // Fill in
+    // For loop runs forever (break condition needed?)
+    for i := 3; i<5*N; i += 2 {
+        out<- i
+    }
+    fmt.Println(2)
+    close(out)
 }
 
-func sieve(in <-chan  int, out chan<- int) {
-    // Fill in
+func sieve(in <-chan int, out chan<- int) {
+    // First candidate received is considered
+    // this goroutines prime (should 'ok' be tested before entering forloop?)
+    prime := <-in
+    for {
+        // Get the newest prime candidate
+        cand := <-in
+
+        if cand == 0 {
+            break
+        }
+
+        // If it is not a multiple of goroutines own prime,
+        // send it to the out channel
+        if cand % prime != 0 {
+            out<- cand
+        }
+    }
+    fmt.Println(prime)
+    close(out)
 }
 
 func main() {
     // Declare channels
-
-    // Initialize channels 
+    var chans [N]chan int
+    // Initialize channels
+    for i := 0; i<N; i++ {
+        chans[i] = make(chan int)
+    }
 
     fmt.Println("The first", N, "prime numbers are:");
 
     // Connect/start goroutines
-
+    go odds(chans[0])
+    for i := 1; i<N; i++ {
+        go sieve(chans[i-1], chans[i])
+    }
     // Await termination
+    for {
+        gobble :=  <-chans[N-1]
+        if gobble == 0 {
+            break
+        }
+    }
 
     fmt.Println("Done!")
 }
